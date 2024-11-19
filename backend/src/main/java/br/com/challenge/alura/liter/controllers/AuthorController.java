6cbf2d0fdc45e0eb0c40c6fd0d85a3729abb9951
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2024-11-18 21:39:58 -0300

    feat: controllers books and authors

---
+++ backend/src/main/java/br/com/challenge/alura/liter/controllers/AuthorController.java
@@ -0,0 +1,61 @@
+package br.com.challenge.alura.liter.controllers;
+
+import br.com.challenge.alura.liter.models.entites.Author;
+import br.com.challenge.alura.liter.services.AuthorService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Author>> findAll(Pageable pageable) {
+        final var result = authorService.findAll(pageable);
+        final var nextPageUrl = result.hasNext() ?
+                ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/v1/authors")
+                    .queryParam("page", result.getNumber() +1)
+                    .queryParam("size", "10")
+                    .toUriString()
+                : null;
+        return ResponseEntity.ok()
+                .header("X-Next-Page", nextPageUrl != null ?  nextPageUrl : "")
+                .body(result.getContent());
+    }
+
+    @GetMapping("/alive")
+    public ResponseEntity<List<Author>> findAliveAfterBy(@RequestParam Integer after,
+                                                         Pageable pageable) {
+        final var result = authorService.findAliveAfterBy(after, pageable);
+        final var path = "/v1/authors";
+        final var nextPageUrl = result.hasNext() ?
+                ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path(path + "/alive")
+                    .queryParam("page", result.getNumber() +1)
+                    .queryParam("size", "10")
+                    .toUriString()
+                : ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path(path)
+                    .queryParam("page", "0")
+                    .queryParam("size", "10")
+                    .toUriString();
+        return ResponseEntity.ok()
+                .header("X-Next-Page", nextPageUrl != null ?  nextPageUrl : "")
+                .body(result.getContent());
+    }
+
+}
